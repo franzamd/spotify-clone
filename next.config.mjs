2227300761f86440bb40d0c1fae38e98318444d5
Franz Medrano <franz.medrano@icloud.com>
2024-07-08 16:09:21 -0400

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      'mkskkkzebgyndqizmjzq.supabase.co'
+    ]
+  }
+};
 
 export default nextConfig;
